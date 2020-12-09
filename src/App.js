42685ae3bf0327983a86edba24be9ae74351c912
Shubhashish Verma <svashish305@gmail.com>
2020-12-09 14:40:34 +0530

    rendered header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,40 @@
-import './App.css';
+import React from 'react';
+import './App.scss';
+import { Container, Row, Col, Image, Button } from 'react-bootstrap';
+import LandingPage from './components/LandingPage/landing-page';
 
 function App() {
 	return (
 		<div className='App'>
-			<header className='App-header'>Header</header>
+			<header className='App-header'>
+				<Container>
+					<Row className='flex-center'>
+						<Col>
+							<Image
+								className='logo'
+								src='assets/images/logo.png'
+								alt='shifu'
+							/>
+						</Col>
+						<Col className='header-right'>
+							<Button variant='secondary' className='shop-btn'>
+								Shop
+							</Button>
+							<Image
+								className='header-right-icon'
+								src='assets/images/cart.png'
+								alt='cart'
+							/>
+							<Image
+								className='header-right-icon'
+								src='assets/images/menu.png'
+								alt='menu'
+							/>
+						</Col>
+					</Row>
+				</Container>
+			</header>
+			<LandingPage />
 		</div>
 	);
 }
---
+++ src/components/LandingPage/landing-page.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './landing-page.scss';
+
+function LandingPage() {
+	return (
+		<React.Fragment>
+			<div className='container'></div>
+		</React.Fragment>
+	);
+}
+
+export default LandingPage;
