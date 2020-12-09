c1bddd029d8c5747778739d5d751b6b77e186265
Shubhashish Verma <svashish305@gmail.com>
2020-12-09 16:58:07 +0530

    added custom accordion toggle on card

--- src/components/LandingPage/landing-page.js
+++ src/components/LandingPage/landing-page.js
@@ -1,8 +1,43 @@
-import React from 'react';
-import { Container, Row, Col, Image, Button } from 'react-bootstrap';
+import React, { useContext } from 'react';
+import {
+	Container,
+	Row,
+	Col,
+	Image,
+	Button,
+	Accordion,
+	AccordionContext,
+	Card,
+} from 'react-bootstrap';
+import { useAccordionToggle } from 'react-bootstrap/AccordionToggle';
 import './landing-page.scss';
 
 function LandingPage() {
+	function ContextAwareToggle({ children, eventKey, callback }) {
+		const currentEventKey = useContext(AccordionContext);
+
+		const decoratedOnClick = useAccordionToggle(
+			eventKey,
+			() => callback && callback(eventKey)
+		);
+
+		const isCurrentEventKey = currentEventKey === eventKey;
+
+		return (
+			<Image
+				onClick={decoratedOnClick}
+				className='float-right'
+				src={
+					isCurrentEventKey
+						? 'assets/images/caret-up.svg'
+						: 'assets/images/caret-down.svg'
+				}
+			>
+				{children}
+			</Image>
+		);
+	}
+
 	return (
 		<div className='landing-page'>
 			<Container className='p-0'>
@@ -22,6 +57,25 @@ function LandingPage() {
 				<label className='component2-label1'>Works with plugo app</label>
 				<label className='component2-heading'>Five gaming kits</label>
 			</Container>
+			<Accordion>
+				<Card className='mt-30'>
+					<Card.Body>
+						<hr className='m-0 h-seg green float-left' />
+						<label className='card-subtext float-left'>WORD BUILDING</label>
+						<label className='mt-12 card-text green float-left'>Letters</label>
+						<Image
+							className='card-img'
+							src='assets/images/lp-letters.svg'
+							fluid
+						/>
+						<ContextAwareToggle eventKey='0'></ContextAwareToggle>
+						<Accordion.Collapse eventKey='0'>
+							{/* put expanded content here */}
+							<Card.Body>Hello! This card is now expanded</Card.Body>
+						</Accordion.Collapse>
+					</Card.Body>
+				</Card>
+			</Accordion>
 		</div>
 	);
 }
