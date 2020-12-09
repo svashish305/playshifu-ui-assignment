50aad3f87878ccf08e0a131b0b8c9c532a26343d
Shubhashish Verma <svashish305@gmail.com>
2020-12-09 20:01:04 +0530

    rendered carousel and thumbnail row

--- src/components/LandingPage/landing-page.js
+++ src/components/LandingPage/landing-page.js
@@ -8,6 +8,7 @@ import {
 	Accordion,
 	AccordionContext,
 	Card,
+	Carousel,
 } from 'react-bootstrap';
 import { useAccordionToggle } from 'react-bootstrap/AccordionToggle';
 import './landing-page.scss';
@@ -70,8 +71,75 @@ function LandingPage() {
 						/>
 						<ContextAwareToggle eventKey='0'></ContextAwareToggle>
 						<Accordion.Collapse eventKey='0'>
-							{/* put expanded content (carousel) here */}
-							<Card.Body>Hello! This card is now expanded</Card.Body>
+							<div className='mt-18'>
+								<Carousel className='ml-16'>
+									<Carousel.Item interval={1000}>
+										<Image
+											className='d-block w-100'
+											src='assets/images/letters1-1x.svg'
+											alt='First slide'
+											fluid
+										/>
+									</Carousel.Item>
+									<Carousel.Item interval={1000}>
+										<Image
+											className='d-block w-100'
+											src='assets/images/letters2-1x.svg'
+											alt='Second slide'
+											fluid
+										/>
+									</Carousel.Item>
+									<Carousel.Item interval={1000}>
+										<Image
+											className='d-block w-100'
+											src='assets/images/letters3-1x.svg'
+											alt='Third slide'
+											fluid
+										/>
+									</Carousel.Item>
+									<Carousel.Item interval={1000}>
+										<Image
+											className='d-block w-100'
+											src='assets/images/letters4-1x.svg'
+											alt='Fourth slide'
+											fluid
+										/>
+									</Carousel.Item>
+								</Carousel>
+								<Container className='mt-18'>
+									<Row>
+										<p className='ml-16 gray-text'>
+											Bring alive stories on screen by spelling words, letter by
+											letter, in the real world. The vocabulary buliding and
+											grammer kit comes with :
+										</p>
+										<ul className='gray-text'>
+											<li>48 letter tiles and a trunk to play and store</li>
+											<li>Age-adaptive spellings for Pre-kg to Grade 4</li>
+											<li>6 games inside the Plugo app with 300+ levels</li>
+										</ul>
+									</Row>
+									<Row className='ml-0 d-flex align-items-center'>
+										<Image
+											className='mr-8'
+											src='assets/images/game-thumbnail-1.svg'
+										/>
+										<Image
+											className='mr-8'
+											src='assets/images/game-thumbnail-2.svg'
+										/>
+										{/* add text overlay for counter to below image */}
+										<Image
+											className='more-thumbnail-imgs'
+											src='assets/images/game-modal-3.svg'
+										/>
+										<Button className='open-modal-btn' variant='outline-accent'>
+											Learn More
+										</Button>
+									</Row>
+									<Row className='ml-0 mt-24'>Next row</Row>
+								</Container>
+							</div>
 						</Accordion.Collapse>
 					</Card.Body>
 				</Card>
