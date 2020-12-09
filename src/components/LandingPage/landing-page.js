6ffa23e8ea07572b303e5c34da192ee447d32bb4
Shubhashish Verma <svashish305@gmail.com>
2020-12-09 15:13:21 +0530

    completed till banner of landing page

--- src/components/LandingPage/landing-page.js
+++ src/components/LandingPage/landing-page.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import { Container, Row, Col, Image, Button } from 'react-bootstrap';
 import './landing-page.scss';
 
 function LandingPage() {
 	return (
 		<React.Fragment>
-			<div className='container'></div>
+			<Container className='p-0'>
+				<Image className='plugo-img' src='assets/images/plugo-logo.svg' />
+				<label className='plugo-caption'>
+					One Gamepad
+					<br /> Five Gaming Kits
+				</label>
+				<Image
+					className='video-play-img'
+					src='assets/images/video-play.svg'
+					alt='play video'
+				/>
+				<Image src='assets/images/lp-banner.svg' fluid />
+			</Container>
 		</React.Fragment>
 	);
 }
